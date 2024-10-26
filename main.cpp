6d68ee49d732cc5189cbf289bcb6291bbad83dd6
cen <cenxiaowei2023@163.com>
2024-10-26 22:06:06 +0800

    test

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <queue>
 #include <deque>
 #include <forward_list>
+// #include <priority_queue>
 
 //int main() {
 //    // std::cout << "Hello, World!" << std::endl;
@@ -214,8 +215,6 @@ void fun2() {
     }
 }
 
-
-
 //int main() {
 //    string s1("asd");
 //    string s2("dddd");
@@ -378,12 +377,9 @@ bool deleteOddNumber(int& i) {
 
 // tasks:
 // 1. 离散数学
-// 2. code
-
-
-int main() {
+// 2. codeint main() {
 //    stack<int>s;
-//    // 栈没有迭代器
+//    栈没有迭代器
 //    int a = INTMAX_MAX;
 //    cout << a;
 //    stack<int>s;
@@ -395,7 +391,6 @@ int main() {
 //        cout << s.top() << " ";
 //        s.pop();
 //    }
-
 //    queue<int,vector<int>>q;
 //    q.push(100);
 //    q.push(2);
@@ -406,9 +401,141 @@ int main() {
 //        cout << q.front() << " ";
 //        q.pop();
 //    }
-    deque<int>d;
+//    deque<int>d;
+//    priority_queue<int>q;
+//    q.push(100);
+//    q.push(200);
+//
+//
+//    return 0;
+//}
+//int main() {
+//    priority_queue<int,vector<int>> p;
+//    p.push(100);
+//    p.push(10);
+//    p.push(22);
+//    p.push(34);
+//    p.push(1);
+//    // 100 34 22 10 1
+//    while(!p.empty()) {
+//        cout << p.top() << " ";
+//        p.pop();
+//    }
+//    return 0;
+//}
+
+// 后续内容： 模板进阶 + 继承多态 + 二叉树 + set + map + C++ 11 + 复习
+
+template<typename T, int N>
+T add(T x,T y) {
+    int arr[N];
+    return x + y;
+}
+#include<string.h>
+
+template<typename T>
+bool isEqual(T t1,T t2) {
+    return t1 == t2;
+}
+template<>
+bool isEqual(char* ch1,char* ch2) {
+    return strcmp(ch1,ch2) == 0;
+}
+
+// 类原模版
+template<class T1,class T2>
+class A {
+private:
+    T1 t1;
+    T2 t2;
+public:
+    A(T1 _t1,T2 _t2) : t1(_t1),t2(_t2) {
+        cout << "A <T1, T2>" << endl;
+    };
+};
+// 类模板的特化
+template<>
+class A<int, double> {
+private:
+    int t1;
+    double t2;
+public:
+    A(int _t1,double _t2) : t1(_t1),t2(_t2) {
+        cout << "A <int, double>" << endl;
+    };
+};
+
+// 类模板的偏特化
+// 特化部分参数
+template<class T2>
+class A<int, T2> {
+private:
+    int t1;
+    T2 t2;
+public:
+    A(int _t1,T2 _t2) : t1(_t1),t2(_t2) {
+        cout << "A <int, T2>" << endl;
+    };
+};
+
+// 对参数进行限制
+template<class T1,class T2>
+class A<T1&, T2&> {
+private:
+    T1 t1;
+    T2 t2;
+public:
+    A(T1 _t1,T2 _t2) : t1(_t1),t2(_t2) {
+        cout << "A <T1&, T2&>" << endl;
+    };
+};
+
+//    int main() {
+//    cout << isEqual<int>(100,200);      // 0
+//    cout << isEqual<double>(3.14,3.14); // 1
+//    char ch1[] = "asdf";
+//    char ch2[] = "asdf";
+//    cout << isEqual<char*>(ch1,ch2); // ?
+//    }
+
+//int main() {
+//    A<int,string> a1(12,"wewf");    //A <T1, T2>
+//    A<int,double> a2(12, 3.111);    //A <int, double>
+//    return 0;
+//}
+
+
+// 学习继承
+// 人类、学生类、老师类
+class person {
+private:
+    string name;
+    string gender;
+    int age;
+public:
+    person(string _name,string _gender,int _age) : name(_name),gender(_gender),age(_age) {};
+};
+
+class student : public person{
+private:
+    int stuid;
+public:
+    // ...
+};
+
+class teacher : public person{
+private:
+    int jobid;
+public:
+    // ...
+};
+
+// 10/27 tasks
+// 1.继承的完结
+// 2.线代、离散、概率...
+// 3.英语六级
+
+int main() {
 
     return 0;
 }
-
-
--- stu.cpp
+++ stu.cpp
@@ -2,9 +2,9 @@
 // Created by cen on 2024/10/14.
 //
 
-#include "stu.h"
-
-template<class T>
-void stu<T>::showInfo() {
-    cout << name << age;
-}
+//#include "stu.h"
+//
+//template<class T>
+//void stu<T>::showInfo() {
+//    cout << name << age;
+//}
